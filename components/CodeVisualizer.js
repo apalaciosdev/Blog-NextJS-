b2fed89e50cd62ac659e3006252f6baf7ed09c72
apalacios <apalacios.dev@gmail.com>
2021-12-05 16:38:04 +0100

    2112051637

--- components/CodeVisualizer.js
+++ components/CodeVisualizer.js
@@ -3,6 +3,8 @@ import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.css";
 import styled from "styled-components";
 import px2vw from '../utils/px2vw';
+import Highlight, { defaultProps } from "prism-react-renderer";
+import theme from "prism-react-renderer/themes/nightOwlLight";
 
 
 const Div = styled.div`
@@ -20,16 +22,29 @@ const Pre = styled.pre`
 
 
 
-export const CodeVisualizer = ({ code, language }) => {
-  useEffect(() => {
-    Prism.manual = true;
-    Prism.highlightAll();
-  }, []);
+export const CodeVisualizer = ({ children }) => {
+  const code = children.props.children;
+  const language = children.props.className?.replace("language-", "").trim();
+
   return (
-    <Div>
-      <Pre>
-        <code className={`language-${language}`}>{code}</code>
-      </Pre>
-    </Div>
+    <Highlight {...defaultProps} code={code} language={language}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <CodeSnippetContainer>
+          {language && (
+            <LanguageHeadingContainer>{language.toUpperCase()}</LanguageHeadingContainer>
+          )}
+          <PreBlock className={className} style={{ ...style }}>
+            {tokens.slice(0, -1).map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+            
+          </PreBlock>
+        </CodeSnippetContainer>
+      )}
+    </Highlight>
   );
-}
+};
