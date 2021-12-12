da4d45b8c48fc630b8438c943ebef7d3d0d18528
apalacios <apalacios.dev@gmail.com>
2021-12-12 11:35:44 +0100

    2112121135

--- components/Toggle.js
+++ components/Toggle.js
@@ -1,58 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { func, string } from 'prop-types';
 import styled from "styled-components"
 import Image from 'next/image'
 
 
-
-const ToggleContainer = styled.button`
-  background: ${({ theme }) => theme.gradient};
-  border: 2px solid ${({ theme }) => theme.toggleBorder};
-  border-radius: 30px;
-  cursor: pointer;
-  display: flex;
-  font-size: 0.5rem;
-  justify-content: space-between;
-  margin: 0 auto;
-  overflow: hidden;
-  padding: 0.5rem;
-  position: relative;
-  width: 8rem;
-  height: 4rem;
-
-  svg {
-    height: auto;
-    width: 2.5rem;
-    transition: all 0.3s linear;
-    
-    // sun icon
-    &:first-child {
-      transform: ${({ lightTheme }) => lightTheme ? 'translateY(0)' : 'translateY(100px)'};
-      
-    }
-    
-    // moon icon
-    &:nth-child(2) {
-      transform: ${({ lightTheme }) => lightTheme ? 'translateY(-100px)' : 'translateY(0)'};
-    }
-  }
+const ToggleContainer = styled.div`
+  width: 10px;
+  
 `;
+
 
 
 const Toggle = ({theme,  toggleTheme }) => {
 
-  const [isChecked, setIsChecked] = useState(true);
 
     return (
-      <div>      
-        <input type="checkbox" id="toggle" className="toggle--checkbox" onClick={toggleTheme}
-          onChange={(event) => setIsChecked(event.currentTarget.checked)}
-          checked={theme === "light" ? false : true}
-        />
-        <label for="toggle" class="toggle--label" value="">
-          <span className="toggle--label-background"></span>
-        </label>
-        <div className="background"></div>
+      <div>
+        <ToggleContainer>
+
+       
+        
+
+        
+      
+        
+
+
+        <div class = 'toggle-switch'>
+            <label>
+                <input type = 'checkbox' onClick={toggleTheme} checked={theme === "light" ? true : false}/>
+                <span class = 'slider'></span>
+            </label>
+        </div>
+  
+
+
+        </ToggleContainer>
       </div>
     );
 };
