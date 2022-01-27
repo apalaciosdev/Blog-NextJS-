72963ac3c3144e1710bf203548737340bdabaaea
Aaron Palacios Olea <74265184+apalaciosdev@users.noreply.github.com>
2022-01-27 12:33:22 +0100

    2101271233

--- components/PostListItem.js
+++ components/PostListItem.js
@@ -1,18 +1,46 @@
 import React, { useContext } from "react"
 import Image from "next/image"
+import styled from 'styled-components';
+
+
+const StyledImage = styled(Image)`
+  border-radius: 10px;
+  flex-grow:0;
+  flex-shrink:0;
+  width: 2%;
+  `
+
+const StyledH3 = styled.h3`
+  width: 98%;
+  padding-left: 10px;
+  text-align: left;
+  
+`
+
+const StyledDiv = styled.div`
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  padding-rigth: 10px;
+  padding-left: 10px;
+  width: 99%;
+  &:hover{
+    background: ${props => props.theme === 'light' ? '#f0eeee' :  props => props.theme === 'dark' ? '#182026' : '#f0eeee'}
+  } 
+`
+
 
 export const PostListItem = ({ title, date, languaje }) => {
-
   return (
-    <div className="StyledDiv">
-      <Image
+    <StyledDiv >
+      <StyledImage 
         src={`/images/icons/${languaje}.png`}
         alt="Logo"
         width={40}
         height={40}
         className="StyledImage"
       />
-      <h3 className="StyledH3"> {title} </h3>
-    </div>
+      <StyledH3> {title} </StyledH3>
+    </StyledDiv>
   )
 }
